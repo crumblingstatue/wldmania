f3a8889111569b380ddbcb8e988c41b8aa9e4c08
crumblingstatue <radiantstatue@gmail.com>
2020-05-18 23:39:48 +0200

    Fix clippy lint about 'static

--- src/item_id_pairs.rs
+++ src/item_id_pairs.rs
@@ -1,3 +1,3 @@
 // See `items` folder for credits and license for the item id<->name mappings.
 
-pub static ITEM_ID_PAIRS: &'static [u8] = include_bytes!("../item_id_pairs.bin");
+pub static ITEM_ID_PAIRS: &[u8] = include_bytes!("../item_id_pairs.bin");
