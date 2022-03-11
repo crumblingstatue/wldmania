7f5e26225ef5bf48e5f603aaa8db138a237288bb
crumblingstatue <radiantstatue@gmail.com>
2022-03-12 00:24:56 +0100

    Add a recently used list

--- wldmanip/src/main.rs
+++ wldmanip/src/main.rs
@@ -1,14 +1,52 @@
+use std::path::{Path, PathBuf};
+
+use directories::ProjectDirs;
 use egui_macroquad::{
     egui::{Grid, ScrollArea, TopBottomPanel, Window},
     macroquad,
 };
 
 use macroquad::prelude::*;
+use recently_used_list::RecentlyUsedList;
+use serde::{Deserialize, Serialize};
+use terraria_wld::{Header, WorldFile};
+
+#[derive(Serialize, Deserialize, Default)]
+struct Config {
+    recent_files: RecentlyUsedList<PathBuf>,
+}
+
+impl Config {
+    fn load_or_default() -> anyhow::Result<Self> {
+        let cfg_path = cfg_path();
+        if cfg_path.exists() {
+            let text = std::fs::read_to_string(&cfg_path)?;
+            Ok(serde_json::from_str(&text)?)
+        } else {
+            Ok(Default::default())
+        }
+    }
+    fn save(&self) -> anyhow::Result<()> {
+        std::fs::create_dir_all(cfg_path().parent().unwrap())?;
+        Ok(std::fs::write(
+            cfg_path(),
+            serde_json::to_string_pretty(self)?,
+        )?)
+    }
+}
+
+fn cfg_path() -> PathBuf {
+    let proj_dir = ProjectDirs::from("", "crumblingstatue", "wldmanip").unwrap();
+    let cfg_path = proj_dir.config_dir().join("wldmanip.json");
+    cfg_path
+}
 
 #[macroquad::main("egui with macroquad")]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let mut world = None;
     let mut header = None;
+    let mut cfg = Config::load_or_default()?;
+    prevent_quit();
     loop {
         clear_background(WHITE);
 
@@ -19,19 +57,24 @@ async fn main() {
                 ui.menu_button("File", |ui| {
                     if ui.button("Open").clicked() {
                         if let Some(path) = rfd::FileDialog::new().pick_file() {
-                            match terraria_wld::WorldFile::open(&path, false) {
-                                Ok(mut wld) => {
-                                    header = Some(wld.read_header().unwrap());
-                                    world = Some(wld);
-                                }
-                                Err(e) => {
-                                    rfd::MessageDialog::new()
-                                        .set_description(&e.to_string())
-                                        .show();
-                                }
+                            if load_world(&path, &mut header, &mut world) {
+                                cfg.recent_files.use_(path);
                             }
                         }
                         ui.close_menu();
+                    }
+                    ui.separator();
+                    let mut used = None;
+                    for recent in cfg.recent_files.iter() {
+                        if ui.button(recent.display().to_string()).clicked() {
+                            load_world(recent, &mut header, &mut world);
+                            used = Some(recent.to_owned());
+                            ui.close_menu();
+                            break;
+                        }
+                    }
+                    if let Some(used) = used {
+                        cfg.recent_files.use_(used);
                     }
                 });
             });
@@ -92,6 +135,11 @@ async fn main() {
 
         // Draw things after egui
 
+        if is_quit_requested() {
+            cfg.save()?;
+            return Ok(());
+        }
+
         next_frame().await;
     }
 }
@@ -104,4 +152,20 @@ fn guid_to_hex(guid: &[u8; 16]) -> String {
         write!(&mut s, "{:02x}", byte).unwrap();
     }
     s
+}
+
+fn load_world(path: &Path, header: &mut Option<Header>, world: &mut Option<WorldFile>) -> bool {
+    match terraria_wld::WorldFile::open(path, false) {
+        Ok(mut wld) => {
+            *header = Some(wld.read_header().unwrap());
+            *world = Some(wld);
+            true
+        }
+        Err(e) => {
+            rfd::MessageDialog::new()
+                .set_description(&e.to_string())
+                .show();
+            false
+        }
+    }
 }
