ead38e0623d57ed6de103d1f9353559f67a4cd43
crumblingstatue <radiantstatue@gmail.com>
2026-02-01 09:07:05 +0100

    Factor out egui ui into a separate function

--- wldview/src/main.rs
+++ wldview/src/main.rs
@@ -2,19 +2,21 @@ use std::{
     fs::File,
     ops::Add,
     path::{Path, PathBuf},
+    sync::mpsc::Sender,
 };
 
 use directories::ProjectDirs;
 use egui_file_dialog::FileDialog;
 use egui_macroquad::{
     EguiMqInteg,
-    egui::{Grid, ScrollArea, Spinner, TopBottomPanel, Window},
+    egui::{self, Grid, ScrollArea, Spinner, TopBottomPanel, Window},
     macroquad,
 };
 
 use macroquad::prelude::*;
 use recently_used_list::RecentlyUsedList;
 use serde::{Deserialize, Serialize};
+use terraria_strings::ItemIdMap;
 use terraria_wld::{BaseHeader, Chest, Header, Liquid, Tile};
 
 #[derive(Serialize, Deserialize, Default)]
@@ -78,6 +80,8 @@ struct UiState {
     selected_chest: Option<usize>,
     loading_tiles: bool,
     hide_ui: bool,
+    file_dia: FileDialog,
+    egui_wants_pointer: bool,
 }
 
 #[macroquad::main("egui with macroquad")]
@@ -113,7 +117,6 @@ async fn main() -> anyhow::Result<()> {
     }
     let item_id_map = terraria_strings::item_ids();
     let mut egui_mq = EguiMqInteg::new();
-    let mut file_dia = FileDialog::new();
     loop {
         clear_background(BLACK);
 
@@ -151,177 +154,24 @@ async fn main() -> anyhow::Result<()> {
         let mp = mouse_position();
         let tile_x = f32::floor(mp.0 / scale as f32 - cam_x / scale as f32);
         let tile_y = f32::floor(mp.1 / scale as f32 - cam_y / scale as f32);
-        let mut egui_wants_pointer = false;
+        ui_state.egui_wants_pointer = false;
 
         if !ui_state.hide_ui {
             egui_mq.ui(|_, egui_ctx| {
-                TopBottomPanel::top("top_panel").show(egui_ctx, |ui| {
-                    ui.horizontal(|ui| {
-                        ui.menu_button("File", |ui| {
-                            if ui.button("Open").clicked() {
-                                file_dia.pick_file();
-                            }
-                            ui.separator();
-                            let mut used = None;
-                            ui.menu_button("Recent", |ui| {
-                                for recent in cfg.recent_files.iter() {
-                                    if ui.button(recent.display().to_string()).clicked() {
-                                        load_world(
-                                            recent,
-                                            &mut world_base,
-                                            &mut tiles,
-                                            &mut map_tex,
-                                        );
-                                        used = Some(recent.to_owned());
-                                        break;
-                                    }
-                                }
-                            });
-                            if let Some(used) = used {
-                                cfg.recent_files.use_(used);
-                            }
-                            ui.separator();
-                            ui.checkbox(
-                                &mut cfg.load_most_recent,
-                                "Load most recent file at start",
-                            );
-                            ui.checkbox(&mut cfg.load_tiles_at_start, "Load tiles at start");
-                        });
-                        ui.menu_button("View", |ui| {
-                            ui.checkbox(&mut cfg.draw_center_marker, "Draw center marker");
-                        });
-                    });
-                });
-                file_dia.update(egui_ctx);
-                if let Some(path) = file_dia.take_picked()
-                    && load_world(&path, &mut world_base, &mut tiles, &mut map_tex)
-                {
-                    cfg.recent_files.use_(path);
-                }
-                if let Some(world_base) = &mut world_base {
-                    Window::new("World").show(egui_ctx, |ui| {
-                        ScrollArea::vertical().show(ui, |ui| {
-                            ui.set_height(600.0);
-                            ui.heading("Basic");
-                            Grid::new("basic_info_grid").striped(true).show(ui, |ui| {
-                                ui.label("Version");
-                                ui.label(world_base.base_header.version.to_string());
-                                ui.end_row();
-                                ui.label("Times saved");
-                                ui.label(world_base.base_header.times_saved.to_string());
-                            });
-                            ui.separator();
-                            ui.heading("Header");
-                            if !ui_state.loading_tiles {
-                                if ui.button("Load tiles").clicked() {
-                                    let base_header = world_base.base_header.clone();
-                                    let header = world_base.header.clone();
-                                    let file = world_base.file.try_clone().unwrap();
-                                    let sender = sender.clone();
-                                    std::thread::spawn(move || {
-                                        let ret_val = load_tiles(&file, &base_header, &header);
-                                        sender.send(ret_val).unwrap();
-                                    });
-                                    ui_state.loading_tiles = true;
-                                }
-                            } else {
-                                ui.horizontal(|ui| {
-                                    ui.label("Loading tiles...");
-                                    ui.add(Spinner::new());
-                                });
-                            }
-                            Grid::new("world_base.header_grid")
-                                .striped(true)
-                                .show(ui, |ui| {
-                                    field_macro!(ui, field);
-                                    field!("Name", world_base.header.name);
-                                    field!("Seed", world_base.header.seed);
-                                    field!(
-                                        "Generator version",
-                                        world_base.header.generator_version
-                                    );
-                                    field!("GUID", guid_to_hex(&world_base.header.guid));
-                                    field!("World id", world_base.header.id);
-                                    ui.label("Bounds");
-                                    Grid::new("bounds_grid").striped(true).show(ui, |ui| {
-                                        field_macro!(ui, field2);
-                                        field2!("left", world_base.header.bounds.left);
-                                        field2!("right", world_base.header.bounds.right);
-                                        field2!("top", world_base.header.bounds.top);
-                                        field2!("bottom", world_base.header.bounds.bottom);
-                                    });
-                                    ui.end_row();
-                                    field!(
-                                        "size",
-                                        format!(
-                                            "{}x{}",
-                                            world_base.header.width, world_base.header.height
-                                        )
-                                    );
-                                    field!(
-                                        "Game mode",
-                                        format!(
-                                            "{} ({})",
-                                            game_mode_name(world_base.header.game_mode),
-                                            world_base.header.game_mode
-                                        )
-                                    );
-                                    if let Some(tile) = tiles.get(
-                                        tile_y as usize * world_base.header.width as usize
-                                            + tile_x as usize,
-                                    ) {
-                                        field!("Pointing at", format!("{}, {}", tile_x, tile_y));
-                                        match tile.front {
-                                            Some(id) => field!("Tile", id),
-                                            None => field!("Tile", "[none]"),
-                                        };
-                                        match tile.back {
-                                            Some(id) => field!("Wall", id),
-                                            None => field!("Wall", "[none]"),
-                                        };
-                                        field!(
-                                            "Liquid",
-                                            match tile.liquid {
-                                                None => "[none]",
-                                                Some(Liquid::Water) => "Water",
-                                                Some(Liquid::Honey) => "Honey",
-                                                Some(Liquid::Lava) => "Lava",
-                                            }
-                                        );
-                                    }
-                                    field!("cam x", cam_x);
-                                    field!("cam y", cam_y);
-                                });
-                        })
-                    });
-                    if let Some(index) = ui_state.selected_chest {
-                        let chest: &Chest = &world_base.chests[index];
-                        let chest_name_fmt: String;
-                        let label = if chest.name.is_empty() {
-                            "Chest"
-                        } else {
-                            chest_name_fmt = format!("Chest: {}", chest.name);
-                            &chest_name_fmt
-                        };
-                        Window::new(label).show(egui_ctx, |ui| {
-                            for item in &chest.items {
-                                if item.id != 0 {
-                                    match item_id_map.name_by_id(item.id as u16) {
-                                        Some(name) => {
-                                            ui.label(format!("{} x {}", name, item.stack))
-                                        }
-                                        None => ui.label(format!(
-                                            "Unknown item ({}) x {}",
-                                            item.id, item.stack
-                                        )),
-                                    };
-                                }
-                                ui.end_row();
-                            }
-                        });
-                    }
-                }
-                egui_wants_pointer = egui_ctx.wants_pointer_input();
+                egui_ui(
+                    egui_ctx,
+                    &mut ui_state,
+                    &mut cfg,
+                    &mut world_base,
+                    &mut tiles,
+                    &mut map_tex,
+                    tile_x,
+                    tile_y,
+                    &item_id_map,
+                    cam_x,
+                    cam_y,
+                    &sender,
+                );
             });
             egui_mq.draw();
         }
@@ -365,7 +215,7 @@ async fn main() -> anyhow::Result<()> {
             scale /= 2;
         }
 
-        if !egui_wants_pointer && is_mouse_button_pressed(MouseButton::Left) {
+        if !ui_state.egui_wants_pointer && is_mouse_button_pressed(MouseButton::Left) {
             ui_state.selected_chest = None;
             if let Some(world_base) = &world_base {
                 for (i, chest) in world_base.chests.iter().enumerate() {
@@ -411,6 +261,171 @@ async fn main() -> anyhow::Result<()> {
 
         next_frame().await;
     }
+}
+
+fn egui_ui(
+    egui_ctx: &egui::Context,
+    ui_state: &mut UiState,
+    cfg: &mut Config,
+    world_base: &mut Option<WorldBase>,
+    tiles: &mut Vec<Tile>,
+    map_tex: &mut Option<Texture2D>,
+    tile_x: f32,
+    tile_y: f32,
+    item_id_map: &ItemIdMap,
+    cam_x: f32,
+    cam_y: f32,
+    sender: &Sender<(Vec<Tile>, Image)>,
+) {
+    TopBottomPanel::top("top_panel").show(egui_ctx, |ui| {
+        ui.horizontal(|ui| {
+            ui.menu_button("File", |ui| {
+                if ui.button("Open").clicked() {
+                    ui_state.file_dia.pick_file();
+                }
+                ui.separator();
+                let mut used = None;
+                ui.menu_button("Recent", |ui| {
+                    for recent in cfg.recent_files.iter() {
+                        if ui.button(recent.display().to_string()).clicked() {
+                            load_world(recent, world_base, tiles, map_tex);
+                            used = Some(recent.to_owned());
+                            break;
+                        }
+                    }
+                });
+                if let Some(used) = used {
+                    cfg.recent_files.use_(used);
+                }
+                ui.separator();
+                ui.checkbox(&mut cfg.load_most_recent, "Load most recent file at start");
+                ui.checkbox(&mut cfg.load_tiles_at_start, "Load tiles at start");
+            });
+            ui.menu_button("View", |ui| {
+                ui.checkbox(&mut cfg.draw_center_marker, "Draw center marker");
+            });
+        });
+    });
+    ui_state.file_dia.update(egui_ctx);
+    if let Some(path) = ui_state.file_dia.take_picked()
+        && load_world(&path, world_base, tiles, map_tex)
+    {
+        cfg.recent_files.use_(path);
+    }
+    if let Some(world_base) = world_base {
+        Window::new("World").show(egui_ctx, |ui| {
+            ScrollArea::vertical().show(ui, |ui| {
+                ui.set_height(600.0);
+                ui.heading("Basic");
+                Grid::new("basic_info_grid").striped(true).show(ui, |ui| {
+                    ui.label("Version");
+                    ui.label(world_base.base_header.version.to_string());
+                    ui.end_row();
+                    ui.label("Times saved");
+                    ui.label(world_base.base_header.times_saved.to_string());
+                });
+                ui.separator();
+                ui.heading("Header");
+                if !ui_state.loading_tiles {
+                    if ui.button("Load tiles").clicked() {
+                        let base_header = world_base.base_header.clone();
+                        let header = world_base.header.clone();
+                        let file = world_base.file.try_clone().unwrap();
+                        let sender = sender.clone();
+                        std::thread::spawn(move || {
+                            let ret_val = load_tiles(&file, &base_header, &header);
+                            sender.send(ret_val).unwrap();
+                        });
+                        ui_state.loading_tiles = true;
+                    }
+                } else {
+                    ui.horizontal(|ui| {
+                        ui.label("Loading tiles...");
+                        ui.add(Spinner::new());
+                    });
+                }
+                Grid::new("world_base.header_grid")
+                    .striped(true)
+                    .show(ui, |ui| {
+                        field_macro!(ui, field);
+                        field!("Name", world_base.header.name);
+                        field!("Seed", world_base.header.seed);
+                        field!("Generator version", world_base.header.generator_version);
+                        field!("GUID", guid_to_hex(&world_base.header.guid));
+                        field!("World id", world_base.header.id);
+                        ui.label("Bounds");
+                        Grid::new("bounds_grid").striped(true).show(ui, |ui| {
+                            field_macro!(ui, field2);
+                            field2!("left", world_base.header.bounds.left);
+                            field2!("right", world_base.header.bounds.right);
+                            field2!("top", world_base.header.bounds.top);
+                            field2!("bottom", world_base.header.bounds.bottom);
+                        });
+                        ui.end_row();
+                        field!(
+                            "size",
+                            format!("{}x{}", world_base.header.width, world_base.header.height)
+                        );
+                        field!(
+                            "Game mode",
+                            format!(
+                                "{} ({})",
+                                game_mode_name(world_base.header.game_mode),
+                                world_base.header.game_mode
+                            )
+                        );
+                        if let Some(tile) = tiles.get(
+                            tile_y as usize * world_base.header.width as usize + tile_x as usize,
+                        ) {
+                            field!("Pointing at", format!("{}, {}", tile_x, tile_y));
+                            match tile.front {
+                                Some(id) => field!("Tile", id),
+                                None => field!("Tile", "[none]"),
+                            };
+                            match tile.back {
+                                Some(id) => field!("Wall", id),
+                                None => field!("Wall", "[none]"),
+                            };
+                            field!(
+                                "Liquid",
+                                match tile.liquid {
+                                    None => "[none]",
+                                    Some(Liquid::Water) => "Water",
+                                    Some(Liquid::Honey) => "Honey",
+                                    Some(Liquid::Lava) => "Lava",
+                                }
+                            );
+                        }
+                        field!("cam x", cam_x);
+                        field!("cam y", cam_y);
+                    });
+            })
+        });
+        if let Some(index) = ui_state.selected_chest {
+            let chest: &Chest = &world_base.chests[index];
+            let chest_name_fmt: String;
+            let label = if chest.name.is_empty() {
+                "Chest"
+            } else {
+                chest_name_fmt = format!("Chest: {}", chest.name);
+                &chest_name_fmt
+            };
+            Window::new(label).show(egui_ctx, |ui| {
+                for item in &chest.items {
+                    if item.id != 0 {
+                        match item_id_map.name_by_id(item.id as u16) {
+                            Some(name) => ui.label(format!("{} x {}", name, item.stack)),
+                            None => {
+                                ui.label(format!("Unknown item ({}) x {}", item.id, item.stack))
+                            }
+                        };
+                    }
+                    ui.end_row();
+                }
+            });
+        }
+    }
+    ui_state.egui_wants_pointer = egui_ctx.wants_pointer_input();
 }
 
 fn rect_contains_point<T: PartialOrd + Add<Output = T> + Copy>(
